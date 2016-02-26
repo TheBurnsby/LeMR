7692f0e431a8956deb613f15a8877d725f04e890
thehappyginger <mcooper@Michaels-MacBook-Pro.local>
2016-02-25 20:47:49 -0700

    Doing some work

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Task;
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Routes File
@@ -11,9 +13,27 @@
 |
 */
 
+/**
+ * Display All Tasks
+ */
 Route::get('/', function () {
-    return view('welcome');
+    //
 });
+
+/**
+ * Add A New Task
+ */
+Route::post('/task', function (Request $request) {
+    //
+});
+
+/**
+ * Delete An Existing Task
+ */
+Route::delete('/task/{id}', function ($id) {
+    //
+});
+
 
 /*
 |--------------------------------------------------------------------------
